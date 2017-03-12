5fcddbd7c5ae53c6b736114935eb7a9a5fe3c4ea
Sean Ho <seansinflipflops@gmail.com>
2017-03-11 19:31:38 -0500

    implemented everything else

---
+++ server/database/connection.js
@@ -0,0 +1,12 @@
+var mysql      = require('mysql');
+ var config = mysql.createConnection({
+  host     : 'fedora-nyc1.laulabs.net',
+   user     : 'narmin',
+   password : 'narmin123',
+   database : 'events_manager',
+   port: 3306
+ });
+
+ config.connect();
+
+ module.exports = config; 
